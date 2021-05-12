adb9f5fd7e4b9f59c728fc214ede9b7c38cfcd4d
martin-taylor <martin.g.taylor@btinternet.com>
2021-05-12 14:36:59 +0100

    Initial Commit

---
+++ basicform/app.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template, request
+from flask_wtf import FlaskForm
+from wtforms import StringField, DateField, IntegerField, DecimalField, SelectField, SubmitField
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'YOUR_SECRET_KEY'
+
+class BasicForm(FlaskForm):
+    first_name = StringField('First Name')
+    last_name = StringField('Last Name')
+    birthday = DateField("Birthday")
+    age = IntegerField("Age")
+    salary = DecimalField("Salary", places=2)
+    submit = SubmitField('Add Name')
+    food = SelectField(choices=['Indian', 'Chinese', 'Italian', 'Chips'])
+
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/home', methods=['GET', 'POST'])
+def register():
+    error = ""
+    form = BasicForm()
+
+    if request.method == 'POST':
+        first_name = form.first_name.data
+        last_name = form.last_name.data
+        birthday = form.birthday.data
+        age = form.age.data
+        salary = form.salary.data
+        food = form.food.data
+
+        if len(first_name) == 0 or len(last_name) == 0:
+            error = "Please supply both first and last name"
+        else:
+            return 'Thank you ' + first_name + " " + last_name + ". Have some " + food
+
+    return render_template('home.html', form=form, message=error)
+
+if __name__ == '__main__':
+     app.run(debug=True, host='0.0.0.0')
