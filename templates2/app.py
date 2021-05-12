2fc996274843873a2c3f7f8a05298db4270b47cf
martin-taylor <martin.g.taylor@btinternet.com>
2021-05-12 12:08:41 +0100

    Initial Commit

---
+++ templates2/app.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+@app.route("/list")
+def index():
+    return render_template("list.html", users=["ben", "harry", "bob", "jay", "matt", "bill"])
+
+if __name__ == "__main__":
+    app.run(debug=True)
