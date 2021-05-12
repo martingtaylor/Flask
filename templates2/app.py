0b670de21f739cddc7a2a504a153c68b4c4730cf
martin-taylor <martin.g.taylor@btinternet.com>
2021-05-12 12:22:57 +0100

    Added more

--- templates2/app.py
+++ templates2/app.py
@@ -5,7 +5,8 @@ app = Flask(__name__)
 @app.route("/")
 @app.route("/list")
 def index():
-    return render_template("list.html", users=["ben", "harry", "bob", "jay", "matt", "bill"])
+    return render_template("list.html", users=["ben", "harry", "bob", "jay", "matt", "bill"], tod="Morning")
 
 if __name__ == "__main__":
     app.run(debug=True)
+    
