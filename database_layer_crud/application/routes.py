36f35a65470fbd9f5b321188ebd33060cc5aaf6c
martin-taylor <martin.g.taylor@btinternet.com>
2021-05-12 10:42:47 +0100

    Inital Commit

---
+++ database_layer_crud/application/routes.py
@@ -0,0 +1,36 @@
+from application import app, db
+from application.models import Games
+
+@app.route('/add')
+def add():
+    new_game = Games(name="New Game")
+    db.session.add(new_game)
+    db.session.commit()
+    return "Added new game to database"
+
+@app.route('/read')
+def read():
+    all_games = Games.query.all()
+    games_string = ""
+    for game in all_games:
+        games_string += "<br>"+ game.name
+    return games_string
+
+@app.route('/update/<name>')
+def update(name):
+    first_game = Games.query.first()
+    first_game.name = name
+    db.session.commit()
+    return first_game.name
+
+@app.route('/delete')
+def delete():
+    del_game = Games.query.first()
+    db.session.delete(del_game)
+    db.session.commit()
+    return "First Game Deleted"
+
+@app.route("/count")
+def count():
+    return "Number of Games " + str(Games.query.count())
+
