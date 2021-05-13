33fe33846e52284c1e686a1bc63adf3a29fd40aa
martin-taylor <martin.g.taylor@btinternet.com>
2021-05-13 10:12:30 +0100

    Added Special Character Check

--- validators_tutorial/app.py
+++ validators_tutorial/app.py
@@ -18,11 +18,25 @@ class UserCheck:
         if field.data.lower() in (word.lower() for word in self.banned):
             raise ValidationError(self.message)
 
+class SpecialCharacters:
+    def __init__(self, banned, message=None):# Here we set up the class to have the banned and message attributes. banned must be passed through at declaration.
+        self.banned = banned
+        if not message:
+            message = 'You cannot use special characters' # If no message chosen, then this default message is returned.
+        self.message = message
+
+    def __call__(self, form, field):
+    # Here we define the method that is ran when the class is called. If the data in our field is in the list of words then raise a ValidationError object with a message.
+        for sc in self.banned:
+            if sc in field.data:
+                raise ValidationError(self.message)
+
 class myForm(FlaskForm):
     username = StringField('Username', validators=[
         DataRequired(),
         # We call our custom validator here, and pass through a message to override the default one. We pass through the list of banned usernames as a list.
-        UserCheck(message="custom rejection message",banned = ['root','admin','sys']),
+        UserCheck(message="Choose another name please",banned = ['root','admin','sys','martin']),
+        SpecialCharacters(message="You can't use special characaters", banned = [';', ':', '@']),
         Length(min=2,max=15)
         ])
     submit = SubmitField('Sign up')
@@ -38,4 +52,3 @@ def postName():
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
-    
